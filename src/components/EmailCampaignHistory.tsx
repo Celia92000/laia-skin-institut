e29df4c1a8e7987ef8dce23f9ad2d0c043c65e61
Celia <celia@laiaskin.com>
2025-10-11 05:30:14 +0200

    Fix: Add useEffect import to EmailCampaignHistory

--- src/components/EmailCampaignHistory.tsx
+++ src/components/EmailCampaignHistory.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Mail, Send, Eye, MousePointer, XCircle, CheckCircle,
   AlertCircle, Calendar, Users, TrendingUp, Filter,
